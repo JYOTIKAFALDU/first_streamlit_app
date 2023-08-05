f9b65b5da96851e58dc6f71283a570a148d161f2
Jyotika_faldu <131469840+JYOTIKAFALDU@users.noreply.github.com>
2023-08-05 14:39:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
-
+requirements.txt
 import snowflake.connector
 
 
